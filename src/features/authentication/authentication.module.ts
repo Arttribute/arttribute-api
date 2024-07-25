2995db4e5fa71bca1a3efef8ee8466838c06ee28
Gitau Gakwa <ggakwa.dev@gmail.com>
2024-07-25 13:43:36 +0300

    Fixed User Module dependency

--- src/features/authentication/authentication.module.ts
+++ src/features/authentication/authentication.module.ts
@@ -1,11 +1,11 @@
-import { Global, Module } from '@nestjs/common';
+import { forwardRef, Global, Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 import { UserModule } from '../user';
 
 @Global()
 @Module({
-  imports: [UserModule],
+  imports: [forwardRef(() => UserModule)],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
   exports: [AuthenticationService],
